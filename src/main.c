6cfd12fbd30da6237d8e430b1f94952dc73bb158
Joseph Ryan <josephryan3.14@gmail.com>
2019-11-24 16:01:53 -0600

    Add test code for servos

---
+++ src/main.c
@@ -0,0 +1,154 @@
+#include "tivaware/gpio.h"
+#include "tivaware/hw_ints.h"
+#include "tivaware/hw_memmap.h"
+#include "tivaware/pin_map.h"
+#include "tivaware/pwm.h"
+#include "tivaware/rom.h"
+#include "tivaware/sysctl.h"
+#include "tivaware/timer.h"
+#include "tivaware/uart.h"
+#include <math.h>
+#include <stdint.h>
+#include <stdio.h>
+
+volatile uint32_t delay_timer;
+
+void systick_handler(void) {
+    --delay_timer;
+}
+
+void wait_ms(uint32_t t) {
+    delay_timer = t;
+    ROM_SysTickPeriodSet(80000 - 1);
+    ROM_SysTickIntEnable();
+    while (delay_timer != 0) {}
+    ROM_SysTickIntDisable();
+}
+
+void timer0_init() {
+    ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_TIMER0);
+    ROM_TimerConfigure(TIMER0_BASE, TIMER_CFG_PERIODIC);
+    ROM_TimerLoadSet(TIMER0_BASE, TIMER_A, 40000000); // .5 seconds
+    ROM_IntEnable(INT_TIMER0A);
+    ROM_TimerIntEnable(TIMER0_BASE, TIMER_TIMA_TIMEOUT);
+    ROM_TimerEnable(TIMER0_BASE, TIMER_BOTH);
+}
+
+void timer0a_handler(void) {
+    ROM_TimerIntClear(TIMER0_BASE, TIMER_A);
+    ROM_GPIOPinWrite(GPIO_PORTA_BASE, GPIO_PIN_7,
+                     ~ROM_GPIOPinRead(GPIO_PORTA_BASE, GPIO_PIN_7));
+}
+
+void pwm_init(void) {
+    ROM_GPIOPinConfigure(GPIO_PB6_M0PWM0);
+    ROM_GPIOPinConfigure(GPIO_PB4_M0PWM2);
+    ROM_GPIOPinConfigure(GPIO_PE4_M0PWM4);
+    ROM_GPIOPinConfigure(GPIO_PE5_M1PWM3);
+    ROM_GPIOPinConfigure(GPIO_PF2_M1PWM6);
+    ROM_GPIOPinTypePWM(GPIO_PORTB_BASE, GPIO_PIN_4 | GPIO_PIN_6);
+    ROM_GPIOPinTypePWM(GPIO_PORTE_BASE, GPIO_PIN_4 | GPIO_PIN_5);
+    ROM_GPIOPinTypePWM(GPIO_PORTF_BASE, GPIO_PIN_2);
+    ROM_PWMGenConfigure(PWM0_BASE, PWM_GEN_0, PWM_GEN_MODE_DBG_RUN);
+    ROM_PWMGenConfigure(PWM0_BASE, PWM_GEN_1, PWM_GEN_MODE_DBG_RUN);
+    ROM_PWMGenConfigure(PWM0_BASE, PWM_GEN_2, PWM_GEN_MODE_DBG_RUN);
+    ROM_PWMGenConfigure(PWM1_BASE, PWM_GEN_1, PWM_GEN_MODE_DBG_RUN);
+    ROM_PWMGenConfigure(PWM1_BASE, PWM_GEN_3, PWM_GEN_MODE_DBG_RUN);
+    ROM_PWMGenPeriodSet(PWM0_BASE, PWM_GEN_0, 25000); // 20ms period for servos
+    ROM_PWMGenPeriodSet(PWM0_BASE, PWM_GEN_1, 25000);
+    ROM_PWMGenPeriodSet(PWM0_BASE, PWM_GEN_2, 12500); // 100Hz for LEDs
+    ROM_PWMGenPeriodSet(PWM1_BASE, PWM_GEN_1, 1420);  // 880Hz for buzzer
+    ROM_PWMGenPeriodSet(PWM1_BASE, PWM_GEN_3, 12500);
+    ROM_PWMPulseWidthSet(PWM0_BASE, PWM_OUT_0, 1875); // 1.5ms for servo pulse
+    ROM_PWMPulseWidthSet(PWM0_BASE, PWM_OUT_2, 1875);
+    // TODO: check if 0 width by default
+    ROM_PWMOutputState(PWM0_BASE, PWM_OUT_0_BIT | PWM_OUT_2_BIT | PWM_OUT_4_BIT,
+                       true);
+    ROM_PWMOutputState(PWM1_BASE, PWM_OUT_3_BIT | PWM_OUT_6_BIT, true);
+    ROM_PWMGenEnable(PWM0_BASE, PWM_GEN_0);
+    ROM_PWMGenEnable(PWM0_BASE, PWM_GEN_1);
+    ROM_PWMGenEnable(PWM0_BASE, PWM_GEN_2);
+    ROM_PWMGenEnable(PWM1_BASE, PWM_GEN_1);
+    ROM_PWMGenEnable(PWM1_BASE, PWM_GEN_3);
+}
+
+void uart_init(void) {
+    ROM_GPIOPinConfigure(GPIO_PA0_U0RX);
+    ROM_GPIOPinConfigure(GPIO_PA1_U0TX);
+    ROM_GPIOPinTypeUART(GPIO_PORTA_BASE, GPIO_PIN_1 | GPIO_PIN_0);
+    ROM_UARTConfigSetExpClk(UART0_BASE, ROM_SysCtlClockGet(), 115200,
+                            UART_CONFIG_WLEN_8 | UART_CONFIG_STOP_ONE |
+                                UART_CONFIG_PAR_NONE);
+    ROM_UARTFIFOLevelSet(UART0_BASE, UART_FIFO_TX1_8, UART_FIFO_RX1_8);
+    ROM_UARTFIFOEnable(UART0_BASE);
+    ROM_UARTEnable(UART0_BASE);
+}
+
+void init(void) {
+    // Clocking and peripheral control
+    ROM_SysCtlClockSet(SYSCTL_SYSDIV_2_5 | SYSCTL_USE_PLL | SYSCTL_XTAL_16MHZ |
+                       SYSCTL_OSC_MAIN);
+    ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_PWM0);
+    ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_PWM1);
+    ROM_SysCtlPWMClockSet(SYSCTL_PWMDIV_64);
+    ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_UART0);
+    ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
+    ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOB);
+    ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOC);
+    ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOD);
+    ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOE);
+    ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
+    ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_ADC0);
+    ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_ADC1);
+
+    ROM_SysTickEnable();
+
+    pwm_init();
+
+    uart_init();
+
+    setvbuf(stdout, NULL, _IONBF, 0); // disable buffered output
+}
+
+// pos ∈ [-1,1]
+void set_x(float pos) {
+    ROM_PWMPulseWidthSet(PWM0_BASE, PWM_OUT_0, 1875 + pos * 625);
+}
+
+// pos ∈ [-1,1]
+void set_y(float pos) {
+    ROM_PWMPulseWidthSet(PWM0_BASE, PWM_OUT_2, 1875 + pos * 625);
+}
+
+// brightness ∈ [0,1]
+void set_blue(float brightness) {
+    ROM_PWMPulseWidthSet(PWM0_BASE, PWM_OUT_4, brightness * 12500);
+}
+
+// brightness ∈ [0,1]
+void set_red(float brightness) {
+    ROM_PWMPulseWidthSet(PWM1_BASE, PWM_OUT_6, brightness * 12500);
+}
+
+void blink_blue(void) {
+    set_blue(0.5);
+    wait_ms(100);
+    set_blue(0);
+}
+
+int main(void) {
+    init();
+    float xpos = 0;
+    float ypos = 0;
+    while (1) {
+        blink_blue();
+        char c = ROM_UARTCharGet(UART0_BASE);
+        switch (c) {
+        case 'w': set_y(ypos = fmax(1, ypos + 0.1)); break;
+        case 'a': set_x(xpos = fmin(-1, xpos - 0.1)); break;
+        case 's': set_y(ypos = fmin(-1, ypos - 0.1)); break;
+        case 'd': set_x(xpos = fmax(1, xpos + 0.1)); break;
+        }
+        printf("%f, %f\n\r", xpos, ypos);
+    }
+}
