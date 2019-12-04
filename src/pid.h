6406a7f3861a147e255068f9820a71c3498f4091
jspspike <jspspike@gmail.com>
2019-12-03 22:16:06 -0600

    Added pid

---
+++ src/pid.h
@@ -0,0 +1,12 @@
+struct pid {
+    float pconst;
+    float iconst;
+    float dconst;
+    float sum;
+    float last;
+    float interval;
+} typedef pid;
+
+pid pid_init(float p, float i, float d, float interval);
+
+float update(pid* p, float error);
